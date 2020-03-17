a4de55bf4ec3afc1cdb8b521a97ce2eb0f8e185a
Svetoslav Petsev <svetoslav_petsev@users.noreply.github.com>
2020-03-18 01:08:46 +0200

    Add project files.

---
+++ P01.Username/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P01.Username
+{
+    class Program
+    {
+        static void Main()
+        {
+            string username = Console.ReadLine();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Sign up")
+                {
+                    break;
+                }
+                string[] cmdCollection = input.Split();
+                string cmd = cmdCollection[0];
+
+                if (cmd == "Case")
+                {
+                    string type = cmdCollection[1];
+                    if (type == "lower")
+                    {
+                        username = username.ToLower();
+                        Console.WriteLine(username);
+                    }
+                    else if (type == "upper")
+                    {
+                        username = username.ToUpper();
+                        Console.WriteLine(username);
+                    }
+                }
+                else if (cmd == "Reverse")
+                {
+                    int startIndex = int.Parse(cmdCollection[1]);
+                    int endInex = int.Parse(cmdCollection[2]);
+                    if (startIndex >= 0 && endInex < username.Length)
+                    {
+                        int count = endInex - startIndex + 1;
+                        string substr = username.Substring(startIndex, count);
+                        string rev = new string(substr.ToCharArray().Reverse().ToArray());
+                        Console.WriteLine(rev);
+                    }
+                }
+                else if (cmd == "Cut")
+                {
+                    string substr = cmdCollection[1];
+                    if (username.Contains(substr))
+                    {
+                        username = username.Replace(substr, string.Empty);
+                        Console.WriteLine(username);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The word {username} doesn't contain {substr}.");
+                    }
+                }
+                else if (cmd == "Replace")
+                {
+                    char symbolToReplace = char.Parse(cmdCollection[1]);
+                    username = username.Replace(symbolToReplace, '*');
+                    Console.WriteLine(username);
+                }
+                else if (cmd == "Check")
+                {
+                    char symbol = char.Parse(cmdCollection[1]);
+                    if (username.Contains(symbol))
+                    {
+                        Console.WriteLine("Valid");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Your username must contain {symbol}.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ P03.Followers/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P03.Followers
+{
+    public class Status
+    {
+        public int CountLike { get; set; }
+        public int CountComment { get; set; }
+
+        public Status(int countLike, int countComent)
+        {
+            this.CountLike = countLike;
+            this.CountComment = countComent;
+        }
+        public int Total => this.CountLike + this.CountComment;
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, Status> peshoFollowers = new Dictionary<string, Status>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Log out")
+                {
+                    break;
+                }
+                string[] cmdCollection = input.Split(": ");
+                string cmd = cmdCollection[0];
+                string username = cmdCollection[1];
+                if (cmd == "New follower")
+                {
+                    if (!peshoFollowers.ContainsKey(username))
+                    {
+                        peshoFollowers.Add(username, new Status(0, 0));
+                    }
+                }
+                else if (cmd == "Like")
+                {
+                    int count = int.Parse(cmdCollection[2]);
+                    if (!peshoFollowers.ContainsKey(username))
+                    {
+                        peshoFollowers.Add(username, new Status(count, 0));
+                    }
+                    else
+                    {
+                        peshoFollowers[username].CountLike += count;
+                    }
+                }
+                else if (cmd == "Comment")
+                {
+                    if (!peshoFollowers.ContainsKey(username))
+                    {
+                        peshoFollowers.Add(username, new Status(0, 1));
+                    }
+                    else
+                    {
+                        peshoFollowers[username].CountComment++;
+                    }
+                }
+                else if (cmd == "Blocked")
+                {
+                    if (!peshoFollowers.ContainsKey(username))
+                    {
+                        Console.WriteLine($"{username} doesn't exist."); ;
+                    }
+                    else
+                    {
+                        peshoFollowers.Remove(username);
+                    }
+                }
+            }
+            Console.WriteLine($"{peshoFollowers.Count} followers");
+            foreach (var users in peshoFollowers.OrderByDescending(x => x.Value.CountLike).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{users.Key}: {users.Value.Total}");
+            }
+        }
+    }
+}
